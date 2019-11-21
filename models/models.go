302a5433fcb1b8447f287ed5b8608c1db47754c5
朱文骁 <34543176+zwx19981207@users.noreply.github.com>
2019-11-21 15:57:26 +0800

    更新结构

--- models/models.go
+++ models/models.go
@@ -4,6 +4,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 func init() {
@@ -17,19 +18,25 @@ func init() {
 }
 
 type BlogArticle struct {
-	Id          int64 `orm:"auto;index;pk"`
-	Author      string
-	Top         bool
-	Title       string
-	Content     orm.TextField
-	Morecontent orm.TextField
-	Category    string
-	Addtime     orm.DateTimeField `orm:"null"`
+	Id          int64       `orm:"column(id);auto"`
+	Author      string    `orm:"column(author);size(255)"`
+	Top         bool      `orm:"column(top)"`
+	Title       string    `orm:"column(title);size(255)"`
+	Content     orm.TextField    `orm:"column(content)"`
+	Morecontent orm.TextField    `orm:"column(morecontent)"`
+	Category    string    `orm:"column(category);size(255)"`
+	Addtime     time.Time `orm:"column(addtime);type(datetime);null;auto_now_add"`
 }
 
 type Admin struct {
-	Username string `orm:"pk"`
-	Pappwd   string
-	Headimg  string
-	Email    string
+	Username      string    `orm:"column(username);pk"`
+	Pappwd  string `orm:"column(pappwd);size(255)"`
+	Headimg string `orm:"column(headimg);size(255)"`
+	Email   string `orm:"column(email);size(255)"`
+}
+
+type Session struct {
+	Id            int    `orm:"column(session_key);pk"`
+	SessionData   string `orm:"column(session_data);null"`
+	SessionExpiry uint   `orm:"column(session_expiry)"`
 }
